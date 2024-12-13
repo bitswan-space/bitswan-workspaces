a88241e43cf22094a505c7dbb82abab53e8c419e
Lukas Vecerka <vecerkalukas18@gmail.com>
2024-12-13 11:21:56 +0100

    implemented cleanup of caddy and gitops on init fail

--- cmd/init.go
+++ cmd/init.go
@@ -39,21 +39,36 @@ func newInitCmd() *cobra.Command {
 	return cmd
 }
 
+func cleanup(dir string) {
+	if err := os.RemoveAll(dir); err != nil {
+		fmt.Printf("Failed to clean up directory %s: %s\n", dir, err)
+	}
+}
+
 func (o *initOptions) run(cmd *cobra.Command, args []string) error {
 	bitswanConfig := os.Getenv("HOME") + "/.config/bitswan/"
 
 	if _, err := os.Stat(bitswanConfig); os.IsNotExist(err) {
 		if err := os.MkdirAll(bitswanConfig, 0755); err != nil {
-			return fmt.Errorf("failed to create BitSwan config directory: %w", err)
+			return fmt.Errorf("Failed to create BitSwan config directory: %w", err)
 		}
 	}
 
 	// Init shared Caddy if not exists
 	caddyConfig := bitswanConfig + "caddy"
+
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println(r)
+			fmt.Println("Failed to start Caddy. Cleaning up...")
+			cleanup(caddyConfig)
+		}
+	}()
+
 	if _, err := os.Stat(caddyConfig); os.IsNotExist(err) {
 		fmt.Println("Setting up Caddy...")
 		if err := os.MkdirAll(caddyConfig, 0755); err != nil {
-			return fmt.Errorf("failed to create Caddy config directory: %w", err)
+			return fmt.Errorf("Failed to create Caddy config directory: %w", err)
 		}
 
 		// Create Caddyfile with email and modify admin listener
@@ -65,22 +80,22 @@ func (o *initOptions) run(cmd *cobra.Command, args []string) error {
 
 		caddyfilePath := caddyConfig + "/Caddyfile"
 		if err := os.WriteFile(caddyfilePath, []byte(caddyfile), 0644); err != nil {
-			return fmt.Errorf("failed to write Caddyfile: %w", err)
+			panic(fmt.Errorf("Failed to write Caddyfile: %w", err))
 		}
 
 		caddyDockerCompose, err := dockercompose.CreateCaddyDockerComposeFile(caddyConfig, o.certsDir, o.domain)
 		if err != nil {
-			return fmt.Errorf("failed to create Caddy docker-compose file: %w", err)
+			panic(fmt.Errorf("Failed to create Caddy docker-compose file: %w", err))
 		}
 
 		caddyDockerComposePath := caddyConfig + "/docker-compose.yml"
 		if err := os.WriteFile(caddyDockerComposePath, []byte(caddyDockerCompose), 0644); err != nil {
-			return fmt.Errorf("failed to write Caddy docker-compose file: %w", err)
+			panic(fmt.Errorf("Failed to write Caddy docker-compose file: %w", err))
 		}
 
 		err = os.Chdir(caddyConfig)
 		if err != nil {
-			return fmt.Errorf("failed to change directory to Caddy config: %w", err)
+			panic(fmt.Errorf("Failed to change directory to Caddy config: %w", err))
 		}
 
 		caddyProjectName := "bitswan-caddy"
@@ -88,14 +103,14 @@ func (o *initOptions) run(cmd *cobra.Command, args []string) error {
 
 		fmt.Println("Starting Caddy...")
 		if err := caddyDockerComposeCom.Run(); err != nil {
-			return fmt.Errorf("failed to start Caddy: %w", err)
+			panic(fmt.Errorf("Failed to start Caddy: %w", err))
 		}
 
 		// wait 5s to make sure Caddy is up
 		time.Sleep(5 * time.Second)
 		err = caddyapi.InitCaddy()
 		if err != nil {
-			return fmt.Errorf("failed to init Caddy: %w", err)
+			panic(fmt.Errorf("Failed to init Caddy: %w", err))
 		}
 
 		fmt.Println("Caddy started successfully!")
@@ -105,20 +120,20 @@ func (o *initOptions) run(cmd *cobra.Command, args []string) error {
 		caddyCertsDir := caddyConfig + "/certs"
 		if _, err := os.Stat(caddyCertsDir); os.IsNotExist(err) {
 			if err := os.MkdirAll(caddyCertsDir, 0755); err != nil {
-				return fmt.Errorf("failed to create Caddy certs directory: %w", err)
+				panic(fmt.Errorf("Failed to create Caddy certs directory: %w", err))
 			}
 		}
 
 		certsDir := caddyCertsDir + "/" + o.domain
 		if _, err := os.Stat(certsDir); os.IsNotExist(err) {
 			if err := os.MkdirAll(certsDir, 0755); err != nil {
-				return fmt.Errorf("failed to create certs directory: %w", err)
+				panic(fmt.Errorf("Failed to create certs directory: %w", err))
 			}
 		}
 
 		certs, err := os.ReadDir(o.certsDir)
 		if err != nil {
-			return fmt.Errorf("failed to read certs directory: %w", err)
+			panic(fmt.Errorf("Failed to read certs directory: %w", err))
 		}
 
 		for _, cert := range certs {
@@ -131,11 +146,11 @@ func (o *initOptions) run(cmd *cobra.Command, args []string) error {
 
 			bytes, err := os.ReadFile(certPath)
 			if err != nil {
-				return fmt.Errorf("failed to read cert file: %w", err)
+				panic(fmt.Errorf("Failed to read cert file: %w", err))
 			}
 
 			if err := os.WriteFile(newCertPath, bytes, 0644); err != nil {
-				return fmt.Errorf("failed to copy cert file: %w", err)
+				panic(fmt.Errorf("Failed to copy cert file: %w", err))
 			}
 		}
 
@@ -149,12 +164,21 @@ func (o *initOptions) run(cmd *cobra.Command, args []string) error {
 	}
 
 	gitopsConfig := bitswanConfig + gitopsName
+
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println(r)
+			fmt.Println("Failed to initialize GitOps. Cleaning up...")
+			cleanup(gitopsConfig)
+		}
+	}()
+
 	if _, err := os.Stat(gitopsConfig); !os.IsNotExist(err) {
 		return fmt.Errorf("GitOps with this name was already initialized: %s", gitopsName)
 	}
 
 	if err := os.MkdirAll(gitopsConfig, 0755); err != nil {
-		return fmt.Errorf("failed to create GitOps directory: %w", err)
+		return fmt.Errorf("Failed to create GitOps directory: %w", err)
 	}
 
 	// Initialize Bitswan workspace
@@ -164,12 +188,12 @@ func (o *initOptions) run(cmd *cobra.Command, args []string) error {
 
 		fmt.Println("Cloning remote repository...")
 		if err := com.Run(); err != nil {
-			return fmt.Errorf("Failed to clone remote repository: %w", err)
+			panic(fmt.Errorf("Failed to clone remote repository: %w", err))
 		}
 		fmt.Println("Remote repository cloned!")
 	} else {
 		if err := os.Mkdir(gitopsWorkspace, 0755); err != nil {
-			return fmt.Errorf("failed to create GitOps workspace directory %s: %w", gitopsWorkspace, err)
+			panic(fmt.Errorf("Failed to create GitOps workspace directory %s: %w", gitopsWorkspace, err))
 		}
 		com := exec.Command("git", "init")
 		com.Dir = gitopsWorkspace
@@ -177,7 +201,7 @@ func (o *initOptions) run(cmd *cobra.Command, args []string) error {
 		fmt.Println("Initializing git in workspace...")
 
 		if err := com.Run(); err != nil {
-			return fmt.Errorf("Failed to init git in workspace: %w", err)
+			panic(fmt.Errorf("Failed to init git in workspace: %w", err))
 		}
 
 		fmt.Println("Git initialized in workspace!")
@@ -190,13 +214,13 @@ func (o *initOptions) run(cmd *cobra.Command, args []string) error {
 
 	fmt.Println("Setting up GitOps worktree...")
 	if err := worktreeAddCom.Run(); err != nil {
-		return fmt.Errorf("failed to create GitOps worktree: %w", err)
+		panic(fmt.Errorf("Failed to create GitOps worktree: %w", err))
 	}
 
 	// Add repo as safe directory
 	safeDirCom := exec.Command("git", "config", "--global", "--add", "safe.directory", gitopsWorktree)
 	if err := safeDirCom.Run(); err != nil {
-		return fmt.Errorf("failed to add safe directory: %w", err)
+		panic(fmt.Errorf("Failed to add safe directory: %w", err))
 	}
 
 	if o.remoteRepo != "" {
@@ -204,14 +228,14 @@ func (o *initOptions) run(cmd *cobra.Command, args []string) error {
 		emptyCommitCom := exec.Command("git", "commit", "--allow-empty", "-m", "Initial commit")
 		emptyCommitCom.Dir = gitopsWorktree
 		if err := emptyCommitCom.Run(); err != nil {
-			return fmt.Errorf("failed to create empty commit: %w", err)
+			panic(fmt.Errorf("Failed to create empty commit: %w", err))
 		}
 
 		// Push to remote
 		setUpstreamCom := exec.Command("git", "push", "-u", "origin", gitopsName)
 		setUpstreamCom.Dir = gitopsWorktree
 		if err := setUpstreamCom.Run(); err != nil {
-			return fmt.Errorf("failed to set upstream: %w", err)
+			panic(fmt.Errorf("Failed to set upstream: %w", err))
 		}
 	}
 
@@ -219,12 +243,12 @@ func (o *initOptions) run(cmd *cobra.Command, args []string) error {
 
 	gitopsLatestVersion, err := dockerhub.GetLatestDockerHubVersion("https://hub.docker.com/v2/repositories/bitswan/gitops/tags/")
 	if err != nil {
-		return fmt.Errorf("failed to get latest BitSwan GitOps version: %w", err)
+		panic(fmt.Errorf("Failed to get latest BitSwan GitOps version: %w", err))
 	}
 
 	bitswanEditorLatestVersion, err := dockerhub.GetLatestDockerHubVersion("https://hub.docker.com/v2/repositories/bitswan/bitswan-editor/tags/")
 	if err != nil {
-		return fmt.Errorf("failed to get latest BitSwan Editor version: %w", err)
+		panic(fmt.Errorf("Failed to get latest BitSwan Editor version: %w", err))
 	}
 
 	createDockerNetworkCom := exec.Command("docker", "network", "create", "bitswan_network")
@@ -243,12 +267,12 @@ func (o *initOptions) run(cmd *cobra.Command, args []string) error {
 	fmt.Println("Setting up GitOps deployment...")
 	gitopsDeployment := gitopsConfig + "/deployment"
 	if err := os.MkdirAll(gitopsDeployment, 0755); err != nil {
-		return fmt.Errorf("Failed to create deployment directory: %w", err)
+		panic(fmt.Errorf("Failed to create deployment directory: %w", err))
 	}
 
 	err = caddyapi.AddCaddyRecords(gitopsName, o.domain, o.certsDir != "")
 	if err != nil {
-		return fmt.Errorf("Failed to add Caddy records: %w", err)
+		panic(fmt.Errorf("Failed to add Caddy records: %w", err))
 	}
 
 	compose, err := dockercompose.CreateDockerComposeFile(
@@ -260,17 +284,17 @@ func (o *initOptions) run(cmd *cobra.Command, args []string) error {
 		o.domain,
 	)
 	if err != nil {
-		return fmt.Errorf("Failed to create docker-compose file: %w", err)
+		panic(fmt.Errorf("Failed to create docker-compose file: %w", err))
 	}
 
 	dockerComposePath := gitopsDeployment + "/docker-compose.yml"
 	if err := os.WriteFile(dockerComposePath, []byte(compose), 0644); err != nil {
-		return fmt.Errorf("Failed to write docker-compose file: %w", err)
+		panic(fmt.Errorf("Failed to write docker-compose file: %w", err))
 	}
 
 	err = os.Chdir(gitopsDeployment)
 	if err != nil {
-		return fmt.Errorf("Failed to change directory to GitOps deployment: %w", err)
+		panic(fmt.Errorf("Failed to change directory to GitOps deployment: %w", err))
 	}
 
 	fmt.Println("GitOps deployment set up successfully!")
@@ -280,7 +304,7 @@ func (o *initOptions) run(cmd *cobra.Command, args []string) error {
 
 	fmt.Println("Starting BitSwan GitOps...")
 	if err := dockerComposeCom.Run(); err != nil {
-		return fmt.Errorf("failed to start docker-compose: %w", err)
+		panic(fmt.Errorf("failed to start docker-compose: %w", err))
 	}
 
 	fmt.Println("BitSwan GitOps initialized successfully!")
