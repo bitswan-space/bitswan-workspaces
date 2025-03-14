55f265b381b568c2a934bf72b593486f56906d9f
Tomáš Ebert <Tomi.Ebik@seznam.cz>
2025-03-14 08:58:46 +0100

    feat: Simplify code into overriding docker-compose file with reading credentials from metadata file

--- cmd/update.go
+++ cmd/update.go
@@ -3,11 +3,11 @@ package cmd
 import (
 	"fmt"
 	"os"
-	"os/exec"
 	"path/filepath"
 
 	"gopkg.in/yaml.v3"
 
+	"github.com/bitswan-space/bitswan-gitops-cli/internal/dockercompose"
 	"github.com/bitswan-space/bitswan-gitops-cli/internal/dockerhub"
 	"github.com/spf13/cobra"
 )
@@ -21,7 +21,7 @@ func newUpdateCmd() *cobra.Command {
 		Run: func(cmd *cobra.Command, args []string) {
 			gitopsName := args[0]
 			fmt.Printf("Updating Docker image: %s...\n", gitopsName)
-			err := updateGitopsDockerImage(gitopsName)
+			err := updateGitops(gitopsName)
 			if err != nil {
 				fmt.Printf("Error updating Docker image: %v\n", err)
 				return
@@ -29,62 +29,6 @@ func newUpdateCmd() *cobra.Command {
 			fmt.Println("Docker image updated successfully!")
 		},
 	}
-}
-
-// update the docker-compose.yml
-func updateDockerCompose(gitopsName string, gitopsImage string, bitswanEditorImage string) error {
-
-	homeDir, err := os.UserHomeDir()
-	if err != nil {
-		return fmt.Errorf("Failed to get home directory:", err)
-	}
-
-	dockerComposePath := filepath.Join(homeDir, ".config", "bitswan", gitopsName, "deployment", "docker-compose.yml")
-
-	// Step 1: Read the file
-	dockerComposeData, err := os.ReadFile(dockerComposePath)
-	if err != nil {
-		return fmt.Errorf("Failed to read docker-compose.yml:", err)
-	}
-
-	// Step 2: Parse the YAML
-	var composeConfig map[string]interface{}
-	err = yaml.Unmarshal(dockerComposeData, &composeConfig)
-	if err != nil {
-		return fmt.Errorf("Error parsing YAML:", err)
-	}
-
-	// Step 3: Update the image of gitops service
-	if service, ok := composeConfig["services"].(map[string]interface{}); ok {
-		if serviceMap, ok := service[gitopsImage].(map[string]interface{}); ok {
-			if gitopsImage, ok := serviceMap["image"].(string); ok {
-				serviceMap["image"] = gitopsImage
-			}
-		}
-	}
-
-	// Step 4: Update the image of editor service
-	if service, ok := composeConfig["services"].(map[string]interface{}); ok {
-		if serviceMap, ok := service["bitswan-editor-"+gitopsName].(map[string]interface{}); ok {
-			if bitswanEditorImage, ok := serviceMap["image"].(string); ok {
-				serviceMap["image"] = bitswanEditorImage
-			}
-		}
-	}
-
-	// Step 4: Convert back to YAML
-	updatedData, err := yaml.Marshal(&composeConfig)
-	if err != nil {
-		return fmt.Errorf("Error marshalling YAML:", err)
-	}
-
-	// Step 5: Write back to file
-	err = os.WriteFile(dockerComposePath, updatedData, 0644)
-	if err != nil {
-		return fmt.Errorf("Error writing file:", err)
-	}
-
-	return nil
 }
 
 func getLatestImagesVersion() (string, string) {
@@ -103,30 +47,33 @@ func getLatestImagesVersion() (string, string) {
 	return gitopsImage, bitswanEditorImage
 }
 
-func updateGitopsDockerImage(gitopsName string) error {
+func updateGitops(gitopsName string) error {
 	gitopsImage, bitswanEditorImage := getLatestImagesVersion()
+	gitopsConfig := os.Getenv("HOME") + "/.config/bitswan/" + "workspaces/" + gitopsName
 
-	// Update the docker-compose.yml
-	err := updateDockerCompose(gitopsName, gitopsImage, bitswanEditorImage)
+	// Get the domain from the file `~/.config/bitswan/<gitops-name>/deployment/domain`
+	dataPath := filepath.Join(os.Getenv("HOME"), ".config", "bitswan", "workspaces", gitopsName, "metadata.yaml")
+
+	data, err := os.ReadFile(dataPath)
 	if err != nil {
-		panic(fmt.Errorf("failed to update Docker image"))
+		return fmt.Errorf("Failed to read metadata.yaml:", err)
 	}
 
-	fmt.Println("Updated docker-compose.yaml successfully!")
-
-	dir := fmt.Sprintf("~/.config/bitswan/%s/deployment", gitopsName)
-	cmd := exec.Command("sh", "-c", fmt.Sprintf("cd %s && docker-compose pull", dir))
-
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		return fmt.Errorf("failed to update Docker image: %v, output: %s", err, string(output))
+	// Config represents the structure of the YAML file
+	var metadata struct {
+		Domain       string `yaml:"domain"`
+		EditorURL    string `yaml:"editor-url"`
+		GitopsURL    string `yaml:"gitops-url"`
+		GitopsSecret string `yaml:"gitops-secret"`
 	}
 
-	cmd = exec.Command("sh", "-c", fmt.Sprintf("cd %s && docker-compose up -d", dir))
-	output, err = cmd.CombinedOutput()
-	if err != nil {
-		return fmt.Errorf("failed to update Docker image: %v, output: %s", err, string(output))
+	if err := yaml.Unmarshal(data, &metadata); err != nil {
+		return fmt.Errorf("Failed to unmarshal metadata.yaml:", err)
 	}
+
+	// Rewrite the docker-compose file
+	noIde := metadata.EditorURL == ""
+	dockercompose.CreateDockerComposeFile(gitopsConfig, gitopsName, gitopsImage, bitswanEditorImage, metadata.Domain, noIde)
 
 	return nil
 }
