10e00b6d34354bde97aaddf851342b40bf3aec96
Tomáš Ebert <Tomi.Ebik@seznam.cz>
2025-05-05 12:29:23 +0200

    feat: Add open for windows, linux and MacOS

--- cmd/open.go
+++ cmd/open.go
@@ -4,7 +4,8 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"path"
+	"path/filepath"
+	"runtime"
 
 	"github.com/spf13/cobra"
 	"gopkg.in/yaml.v3"
@@ -15,41 +16,63 @@ type MetadataOpen struct {
 }
 
 func newOpenCmd() *cobra.Command {
-	cmd := &cobra.Command{
+	return &cobra.Command{
 		Use:          "open",
 		Short:        "Open the editor for a workspace",
 		Args:         cobra.ExactArgs(1),
 		SilenceUsage: true,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			gitopsPath := path.Join(os.Getenv("HOME"), "/.config/bitswan/workspaces", args[0])
+		RunE:         runOpenCmd,
+	}
+}
 
-			metadataPath := gitopsPath + "/metadata.yaml"
+func runOpenCmd(cmd *cobra.Command, args []string) error {
+	workspacePath := filepath.Join(os.Getenv("HOME"), ".config/bitswan/workspaces", args[0])
+	metadataPath := filepath.Join(workspacePath, "metadata.yaml")
 
-			data, err := os.ReadFile(metadataPath)
-			if err != nil {
-				panic(err)
-			}
-
-			var metadata MetadataOpen
-			err = yaml.Unmarshal(data, &metadata)
-			if err != nil {
-				panic(err)
-			}
-
-			if metadata.EditorURL == nil {
-				fmt.Println("No editor URL found in metadata.yaml")
-				return nil
-			}
-
-			fmt.Printf("Opening editor at: %s\n", *metadata.EditorURL)
-			err = exec.Command("xdg-open", *metadata.EditorURL).Start()
-			if err != nil {
-				panic(err)
-			}
-
-			return nil
-		},
+	metadata, err := loadMetadata(metadataPath)
+	if err != nil {
+		return err
 	}
 
-	return cmd
+	if metadata.EditorURL == nil {
+		return fmt.Errorf("no editor URL found in metadata.yaml")
+	}
+
+	fmt.Printf("Opening editor at: %s\n", *metadata.EditorURL)
+	return openURL(*metadata.EditorURL)
+}
+
+func loadMetadata(metadataPath string) (*MetadataOpen, error) {
+	data, err := os.ReadFile(metadataPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read metadata file: %w", err)
+	}
+
+	var metadata MetadataOpen
+	if err := yaml.Unmarshal(data, &metadata); err != nil {
+		return nil, fmt.Errorf("failed to parse metadata file: %w", err)
+	}
+
+	return &metadata, nil
+}
+
+func openURL(url string) error {
+	var cmd *exec.Cmd
+
+	switch runtime.GOOS {
+	case "windows":
+		cmd = exec.Command("start", url)
+	case "darwin":
+		cmd = exec.Command("open", url)
+	case "linux":
+		cmd = exec.Command("xdg-open", url)
+	default:
+		return fmt.Errorf("unsupported operating system")
+	}
+
+	if err := cmd.Start(); err != nil {
+		return fmt.Errorf("failed to open URL: %w", err)
+	}
+
+	return nil
 }
